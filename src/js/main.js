def203c30422701f38d16ffe48727353feb29767
Robert Laws <roblaws@me.com>
2019-02-22 12:51:35 +0300

    continue with quick introduction section

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,34 @@ $('document').ready(function() {
 $("document").ready(function() {
   $(".two p:not(.basic)").css("font-weight", "bold");
 });
+
+$("document").ready(function() {
+  var newParagraph = $("<p>");
+  newParagraph.append("<em>Greetings to all</em>");
+  $(".three").html(newParagraph);
+});
+
+$("document").ready(function() {
+  $(".three p").prepend("Actor says: ");
+});
+
+$("document").ready(function() {
+  $(".five p").text("<h1>An Example Title</h1>");
+});
+
+$("document").ready(function() {
+  $(".six p").on("mouseover", onMouseOver);
+  $(".six p").on("mouseleave", onMouseLeave);
+});
+function onMouseOver(evt) {
+  $(".six p").css("color", "red");
+}
+function onMouseLeave(evt) {
+  $(".six p").css("color", "black");
+}
+
+$("document").ready(function() {
+  $(".seven button").click(function() {
+    $(".seven #box").animate({left: 500, borderWidth: 5}, 400)
+  });
+});
