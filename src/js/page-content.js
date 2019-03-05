2f28f8bff984c2f813991029ae3fa9aef6b826fa
Robert Laws <roblaws@me.com>
2019-03-05 12:14:33 -0500

    finish advanced filters

--- src/js/page-content.js
+++ src/js/page-content.js
@@ -6,4 +6,8 @@ $("section.two-page-content p:gt(2)").css("color", "green");
 $("section.two-page-content p:first").css("color", "blue");
 // $("section.two-page-content p:not(p:eq(2))").css("color", "yellow");
 
+// advanced selectors
 $("section.three-page-content div.one + p").css("color", "red");
+
+// advanced filters
+$("section.four-page-content p[id^=sam]").css("color", "green");
