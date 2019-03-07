50ee72e052b5fa0864c6d7ad5ac1b8986394cdb0
Robert Laws <roblaws@me.com>
2019-03-07 17:49:17 -0500

    finish events section

--- src/js/events.js
+++ src/js/events.js
@@ -14,3 +14,17 @@ function addSize() {
   size = size + 0.1;
   return size + "rem";
 }
+
+// event helpers
+$(".events-two p:first").hover(function(evt) {
+  $(this).toggleClass("blue");
+});
+
+$(".events-two p:last").mouseenter(function() {
+  $(this).css("color", "red");
+});
+
+// JQuery event object
+$(".events-three input:first").keydown(function(event) {
+  $(".events-three p:first").text(event.which);
+});
